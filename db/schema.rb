8fa5ba0dc22dd954e591034c0706099824e1ddd5
Hong Phuc <lhphuc.ynwa@gmail.com>
2018-07-11 16:29:02 +0700

    Chapter 11: Account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_07_12_030027) do
+ActiveRecord::Schema.define(version: 2018_07_13_020119) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +20,9 @@ ActiveRecord::Schema.define(version: 2018_07_12_030027) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
