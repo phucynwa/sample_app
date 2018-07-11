4da4557c5482e369d41ba1c2f0be0510a6d73b56
Hong Phuc <lhphuc.ynwa@gmail.com>
2018-07-11 16:29:02 +0700

    Chapter 10: Updating users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: {maximum: Settings.max_name_length}
   validates :email, presence: true, length: {maximum: Settings.max_email_length},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.min_password_length}
+  validates :password, presence: true, length: {minimum: Settings.min_password_length}, allow_nil: true
 
   class << self
     def digest string
