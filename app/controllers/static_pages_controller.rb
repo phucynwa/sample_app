42c76f233bfab182d65d4844d9b1b70c9e395d07
phucynwa <lhphuc.ynwa@gmail.com>
2018-07-14 23:19:38 +0700

    Chapter 14: Following users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate page: params[:page], per_page: Settings.per_page
+      @feed_items = current_user.feed.newest.page(params[:page]).per Settings.per_page
     end
   end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag gravatar_url, alt: user.name, class: "gravatar"
   end
+
+  def followed_user id
+    current_user.active_relationships.find_by followed_id: id
+  end
+
+  def build_active_relationships
+    current_user.active_relationships.build
+  end
 end
